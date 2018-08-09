825ce46dd49741df0403925c78d0370e362180cb
angyecastillo <lindacasags@hotmail.com>
2018-08-09 14:51:06 -0500

    subiendo funcion de la camara

--- src/js/registerCorreo.js
+++ src/js/registerCorreo.js
@@ -3,38 +3,38 @@ let enviar = document.getElementById('enviar');
 enviar.addEventListener('click', event =>{
   location.href = 'photo.html';
 });
-// (function() {
-//   emailjs.init('user_bDXyjVhH72GtVYaQohpWX');
-// })();
-// const vue = new Vue({
-//   el: '#app',
-//   data() {
-//     return {
-//       from_name: '',
-//       from_email: '',
-//       message: '',
-//       subject: '',
-//     };
-//   },
-//   methods: {
-//     function () {
-//       let data = {
-//         from_name: this.from_name,
-//         from_email: this.from_email,
-//         message: this.message,
-//         subject: this.subject,
-//       };
+(function() {
+  emailjs.init('user_bDXyjVhH72GtVYaQohpWX');
+})();
+const vue = new Vue({
+  el: '#app',
+  data() {
+    return {
+      from_name: '',
+      from_email: '',
+      message: '',
+      subject: '',
+    };
+  },
+  methods: {
+    enviar() {
+      let data = {
+        from_name: this.from_name,
+        from_email: this.from_email,
+        message: this.message,
+        subject: this.subject,
+      };
 
-//       emailjs.send('gmail', 'wework', data)
-//         .then(function(response) {
-//           if (response.text === 'OK') {
-//             alert('El correo se ha enviado de forma exitosa');
-//           }
-//           console.log('SUCCESS. status=%d, text=%s', response.status, response.text);
-//         }, function(err) {
-//           alert('Ocurrió un problema al enviar el correo');
-//           console.log('FAILED. error=', err);
-//         });
-//     }
-//   }
-// });
+      emailjs.send('gmail', 'wework', data)
+        .then(function(response) {
+          if (response.text === 'OK') {
+            alert('El correo se ha enviado de forma exitosa');
+          }
+          console.log('SUCCESS. status=%d, text=%s', response.status, response.text);
+        }, function(err) {
+          alert('Ocurrió un problema al enviar el correo');
+          console.log('FAILED. error=', err);
+        });
+    }
+  }
+});
