10858c080dc51fe97e81fcc67199a3c643254015
DiLeyRa <ing.dianaleyva16@gmail.com>
2018-08-09 09:30:28 -0500

    maquetado para la foto

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ let welcome = document.getElementById('welcome');
 let contentRegister = document.getElementById('contentRegister');
 let selectionEmployee = document.getElementById('selectionEmployee');
 let enviar = document.getElementById('enviar');
+let contentPhoto = document.getElementById('contentPhoto');
+let btnPhoto = document.getElementById('btnPhoto');
 
 btnRegister.addEventListener('click', event =>{
     welcome.style.display = 'none';
@@ -11,5 +13,10 @@ btnRegister.addEventListener('click', event =>{
 
 enviar.addEventListener('click', event =>{
     contentRegister.style.display = 'none';
+    contentPhoto.style.display = 'block';
+});
+
+btnPhoto.addEventListener('click', event =>{
+    contentPhoto.style.display = 'none';
     selectionEmployee.style.display = 'block';
 });
