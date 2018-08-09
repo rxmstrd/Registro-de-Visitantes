9da3e52d3d913f3702f3b862f10804ee9736f332
angyecastillo <lindacasags@hotmail.com>
2018-08-09 10:19:36 -0500

    subiendo registro por email

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+let btnRegister = document.getElementById('register');
+let welcome = document.getElementById('welcome');
+let contentRegister = document.getElementById('contentRegister');
+let selectionEmployee = document.getElementById('selectionEmployee');
+let enviar = document.getElementById('enviar');
+ btnRegister.addEventListener('click', event =>{
+    welcome.style.display = 'none';
+    contentRegister.style.display = 'block';
+});
+ enviar.addEventListener('click', event =>{
+    contentRegister.style.display = 'none';
+    selectionEmployee.style.display = 'block';
+});
