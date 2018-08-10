354894b13d645e47cff6a9da78040aaf5c80b2b2
MirEnovi <garciaivonne11@gmail.com>
2018-08-09 21:21:34 -0500

    agrega maquetado y flujo en interfaz de usuario

--- src/js/registerCorreo.js
+++ src/js/registerCorreo.js
@@ -4,12 +4,14 @@ const name = document.getElementById('name');
 const secondName = document.getElementById('secondName');
 const subject = document.getElementById('subject');
 const email = document.getElementById('email');
+const enviar = document.getElementById('enviar');
+const comeback = document.getElementById('comeback');
+const comeback2 = document.getElementById('comeback2');
 
 // funcion que guarda los datos del usuario
 const userVisit = () => {
   let date = new Date();
   let userDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}-${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-
   userData = {
     userId: userDate,
     Nombre: name.value,
@@ -21,5 +23,9 @@ const userVisit = () => {
 };
 
 // boton que acciona la funcion UserVisit
-let enviar = document.getElementById('enviar');
 enviar.addEventListener('click', userVisit);
+
+// boton que redirecciona de register al inicio
+comeback.addEventListener('click', event => location.href = '../index.html');
+
+
