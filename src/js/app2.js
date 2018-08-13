7029e30ba6c10f28d674d4e1d383ebf1a8567109
MirEnovi <garciaivonne11@gmail.com>
2018-08-13 03:53:30 -0500

    add view admin, and register visit

--- src/js/app2.js
+++ src/js/app2.js
@@ -1,17 +1,58 @@
-// const name = document.getElementById('name');
-// const getAnfitrion = document.getElementById('getAnfitrion');
+// registro de visitas
+// llamdos al dom
+const getHome = document.getElementById('get-home');
+const adminName = document.getElementById('admin-name');
+const visitTable = document.getElementById('visit-table');
 
-// // boton que redirecciona de employees al inicio
-// comeback2.addEventListener('click', event => location.href = '../index.html');
+// funcion observador
+firebase.auth().onAuthStateChanged((usuario) => {
+  let user = firebase.auth().currentUser;
+  if (user !== null) {
+    console.log(user.email);
+    getVisits(user);
 
-// // funcion que que dirige a foto
-// const getTophoto = () => {
-//   if (name.value) {
-//     location.href = '../../src/views/photo.html';
-//   } else {
-//     swal('Faltan campos por llenar');
-//   }
-// }
+  } else {
+    // User is signed out.
+    firebase.auth().signOut();
+    console.log('sin usuario');
+    location.href = '../index.html';
+  }
+});
 
-// // boton que acciona la funcion anfitrión
-// getAnfitrion.addEventListener('click', getTophoto);
+const getVisits = (user) => {
+  const email = user.email;
+  adminName.innerHTML = email;
+  databaseRef = firebase.database().ref('Visitantes');
+  databaseRef.on('value', (snapshot) => {
+    const data = snapshot.val();
+    let number = 0;
+    for (var key in data) {
+      const dataVisit = data[key];
+      let visitName = dataVisit.Nombre;
+      let visitLastName = dataVisit.Apellido;
+      let visitEmail = dataVisit.correo;
+      let visitSubject = dataVisit.Asunto;
+      let visitId = dataVisit.userId;
+      // console.log(dataVisit);
+      number += 1;
+      visitTable.innerHTML += `
+      <tr>
+        <td>${number}</td>
+        <td>${visitName}</td>
+        <td>${visitLastName}</td>
+        <td>${visitEmail}</td>
+        <td>${visitSubject}</td>
+        <td>Terminal1</td>
+        <td>${visitId}</td>
+      </tr>
+      `;
+    }
+  });
+};
+
+const outUser = () => {
+  firebase.auth().signOut();
+  location.href = '../index.html';
+};
+// boton que redirecciona de employees al inicio
+getHome.addEventListener('click', outUser);
